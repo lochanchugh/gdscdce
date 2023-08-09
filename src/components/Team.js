b74fcbe55fd99c62fd26f8b4710b2b1c68e1f8db
lochanchugh <98405764+lochanchugh@users.noreply.github.com>
2023-08-10 01:01:17 +0530

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,9 +2,9 @@ import React, { Fragment } from 'react';
 import './assets/css/Team.css';
 import TeamCard from './TeamCard';
 import KRITII from './assets/images/TEAM PHOTOS/kriti_kumari_gdsc_lead.png';
-import Avipsa from './assets/images/TEAM PHOTOS/avipsa_pradhan_event_management_lead.png';
-import Aditi from './assets/images/TEAM PHOTOS/aditi_singh_design_team_lead.png';
-import Anand from './assets/images/TEAM PHOTOS/anand_rai_social_media_lead.png';
+import AVIPSA from './assets/images/TEAM PHOTOS/avipsa_pradhan_event_management_lead.png';
+import ADITI from './assets/images/TEAM PHOTOS/aditi_singh_design_team_lead.png';
+import ANAND from './assets/images/TEAM PHOTOS/anand_rai_social_meadia_lead.png';
 import SANDHYA from './assets/images/TEAM PHOTOS/Sandhya Yadav (ui ux design).jpg';
 import ANSHUL from './assets/images/TEAM PHOTOS/Anshul_EventManagement.jpg';
 import DEEPTI from './assets/images/TEAM PHOTOS/DeeptiKaushik_eventManagement.jpg';
@@ -65,7 +65,7 @@ const Team = () => {
                          <TeamCard
                             Mname="Avipsa Pradhan"
                             Mdesc="Event Management Lead"
-                            Mphoto={Avipsa}
+                            Mphoto={AVIPSA}
                             link="https://www.linkedin.com/in/avipsapradhan"
                         />
 
@@ -120,7 +120,7 @@ const Team = () => {
                           <TeamCard
                             Mname="Aditi Sharma"
                             Mdesc="Design Team lead"
-                            Mphoto={Aditi}
+                            Mphoto={ADITI}
                             link="https://www.linkedin.com/in/aditi-sharma-4a39b827b"
                         />
                           <TeamCard
@@ -132,7 +132,7 @@ const Team = () => {
                         <TeamCard
                             Mname="Anand Rai"
                             Mdesc="Social Media Lead"
-                            Mphoto={Anand}
+                            Mphoto={ANAND}
                             link="https://www.linkedin.com/in/anand-rai-384026146"
                         /> 
                          <TeamCard
